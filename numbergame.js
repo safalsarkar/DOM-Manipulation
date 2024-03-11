c9cbc6afc5e06a519358a018bf6d7d21a62def57
safalsarkar <157328412+safalsarkar@users.noreply.github.com>
2024-03-11 16:27:39 -0700

    Add files via upload

--- numbergame.js
+++ numbergame.js
@@ -1,5 +1,6 @@
 
-
+var correctSound = new Audio("http://soundbible.com/grab.php?id=1003&type=wav");
+var incorrectSound = new Audio("http://soundbible.com/grab.php?id=1645&type=wav");
 var counter = 0;
 var randomNumber = randomnumbergenerator();
 
@@ -25,7 +26,13 @@ function show_user_History(guess, feedback) {
 
 function restart_game() {
 
-    location.href = "/Index.html"
+    randomNumber = randomnumbergenerator();
+    document.getElementById("userInput").value = "";
+    document.getElementById("attemptCounter").textContent = "";
+    document.getElementById("Feedback").textContent = "";
+    document.getElementById("History").textContent = "";
+    counter = 0;
+    // location.href = "/Index.html"
 }
 
 function guess_logic() {
@@ -34,17 +41,20 @@ function guess_logic() {
 
     if (player_guess == randomNumber) {
         document.getElementById("Feedback").textContent = "Congratulations! You guessed it!";
-       
+        Feedback.style.backgroundColor = "lightgreen";
+        generate_sound(true);
     }
     else if (player_guess < randomNumber) {
         document.getElementById("Feedback").textContent = "Too low. Try again!";
         Feedback.style.backgroundColor = "red";
-       
+        Annimation();
+        generate_sound(false);
     }
     else {
         document.getElementById("Feedback").textContent = "Too high. Try again!";
         Feedback.style.backgroundColor = "yellow";
-        
+        Annimation();
+        generate_sound(false);
     }
     counterupdate();
     document.getElementById("restart_game").style.display = "block";
@@ -55,16 +65,37 @@ document.getElementById("userInput").addEventListener("keydown", function (event
         guess_logic();
     }
 });
-
 document.getElementById("restart_game").addEventListener("dragstart", function (event) {
     event.dataTransfer.setData("text/plain", null);
 });
 
+document.getElementById("dropZone").addEventListener("dragover", function (event) {
+    event.preventDefault();
+});
 
+document.getElementById("dropZone").addEventListener("drop", function (event) {
+    restart_game();
+    event.preventDefault();
+});
+function Annimation() {
+    var Feedback = document.getElementById("Feedback");
+    Feedback.classList.add("annimation");
+    Feedback.addEventListener("animationend", function () {
+        Feedback.classList.remove("annimation");
+    });
+}
+
+function generate_sound(isCorrect) {
+    if (isCorrect) {
+        correctSound.play();
+    } else {
+        incorrectSound.play();
+    }
+}
 
 document.getElementById("submitGuess").addEventListener("click", function () {
     guess_logic();
 });
 
-document.getElementById("restart_game").addEventListener("click", restart_game);
+
 
